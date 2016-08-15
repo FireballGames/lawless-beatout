0a905818ce50d5ad5d836c5323dd2ad3e7495abb
Dmitry Kutsenko <d2emonium@gmail.com>
2016-08-16 02:24:18 +0300

    Base level

--- src/game/level.py
+++ src/game/level.py
@@ -7,7 +7,6 @@ import config
 import game
 import game.enemy
 
-import pygame
 import random
 
 
@@ -18,16 +17,15 @@ ENEMY_COUNT = 5
 
 class Level(d2game.level.Level):
     def __init__(self, player):
+        d2game.level.Level.__init__(self, player)
         self.background = "{}/{}".format(config.RES_DIR, game.BACKGROUND_IMAGE)
 
-        self.entities = pygame.sprite.Group()
-        self.entities.add(player)
-
-        self.enemies = []
-
+    def generate_enemies(self):
+        enemies = []
         for i in range(ENEMY_COUNT):
             x = random.randint(0, ARENA_SIZE[0]) + ARENA_POS[0]
             y = random.randint(0, ARENA_SIZE[1]) + ARENA_POS[1] - game.enemy.ENEMY_SIZE[1]
             enemy = game.enemy.Enemy(x, y)
-            self.entities.add(enemy)
-            self.enemies.append(enemy)
+            enemies.append(enemy)
+
+        return enemies
