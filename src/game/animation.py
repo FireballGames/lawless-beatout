7b4bc2f88e4eee9b28e05f8c5ffe9c9b7ede6a1c
Dmitry Kutsenko <d2emonium@gmail.com>
2016-08-21 16:50:22 +0300

    New stand animation

--- src/game/animation.py
+++ src/game/animation.py
@@ -6,13 +6,16 @@ import pyganim
 
 
 DELAY = 100
-RIGHT = [(resource.HERO, DELAY)]
-LEFT = [(resource.HERO, DELAY)]
+# RIGHT = [(resource.HERO, DELAY)]
+# LEFT = [(resource.HERO, DELAY)]
 JUMP_RIGHT = [(resource.HERO, DELAY)]
 JUMP_LEFT = [(resource.HERO, DELAY)]
 JUMP = [(resource.HERO, DELAY)]
-STAY = [(resource.HERO, DELAY)]
 
-allRect = (0, 0, resource.HERO_SIZE[0], resource.HERO_SIZE[1])
-allImages = pyganim.getImagesFromSpriteSheet(resource.HERO, rows=1, cols=4, rects=[allRect,]) #, width=resource.HERO_SIZE[0], height=resource.HERO_SIZE[1])
-MOVE = list(zip(allImages, [DELAY] * len(allImages)))
+moveRect = (0, 0, resource.HERO_SIZE[0], resource.HERO_SIZE[1])
+moveImages = pyganim.getImagesFromSpriteSheet(resource.HERO_WALK, rows=1, cols=4, rects=[moveRect,]) #, width=resource.HERO_SIZE[0], height=resource.HERO_SIZE[1])
+MOVE = list(zip(moveImages, [DELAY] * len(moveImages)))
+
+stayImages = pyganim.getImagesFromSpriteSheet(resource.HERO_STAY, rows=1, cols=4, rects=[moveRect,])
+STAY = list(zip(stayImages, [250] * len(stayImages)))
+
