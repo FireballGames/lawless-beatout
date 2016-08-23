420a722c81f0c8bcfb700da8547d2af48f101429
Dmitry Kutsenko <d2emonium@gmail.com>
2016-08-23 16:05:18 +0300

    zoo

--- src/d2gui/__init__.py
+++ src/d2gui/__init__.py
@@ -14,19 +14,36 @@ DIR_KEYS = {
 }
 
 
-def background(display):
-    bg = pygame.Surface(display) # Создание видимой поверхности
-                          # будем использовать как фон
+def load_image(name):
+    try:
+        image = pygame.image.load(name)
+    except pygame.error, message:
+        print("Can't load image: {}".format(name))
+        raise SystemExit, message
+    image = image.convert_alpha()
+    return image, image.get_rect()
+
+
+def background(background):
+    screen = pygame.display.get_surface()
+    bg = pygame.Surface(screen.get_size()) # (display) # Создание видимой поверхности
+    bg = bg.convert()
     # bg.fill(pygame.Color(config.BACKGROUND_COLOR)) # Заливаем поверхность сплошным цветом
-    return bg
+    bg.fill((0, 0, 0))
+    screen.blit(bg, (0, 0))
+    back, back_rect = load_image(background)
+    screen.blit(back, (0, 0))
+    pygame.display.flip()
+    return back
 
 
 class GUI():
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode(config.DISPLAY)
+        self.window = pygame.display.set_mode(config.DISPLAY)
         pygame.display.set_caption(game.TITLE)
-        self.bg = background(config.DISPLAY)
+
+        # self.bg = background(config.DISPLAY)
         self.entities = None # pygame.sprite.Group()
         self.timer = pygame.time.Clock()
         self.game = None
@@ -35,20 +52,20 @@ class GUI():
     def set_game(self, g):
         self.game = g
         self.player = self.game.hero
-
         # self.entities.add(self.player)
 
     def draw(self):
-        self.bg = pygame.image.load(self.game.level.background)
+        # self.bg = pygame.image.load(self.game.level.background)
 
-        self.screen.blit(self.bg, (0,0))
-        self.game.level.entities.draw(self.screen)
+        # self.window.blit(self.bg, (0,0))
+        self.bg = background(self.game.level.background) # config.DISPLAY)
+        self.game.level.entities.draw(self.window)
 
         pygame.display.update()     # обновление и вывод всех изменений на экран
         self.timer.tick(30)
 
     def process_events(self):
-        for e in pygame.event.get(): # Обрабатываем события
+        for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 self.game.quit()
             if e.type == pygame.KEYDOWN:
